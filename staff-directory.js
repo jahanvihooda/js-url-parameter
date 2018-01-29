d6a9a04a621e83f7207f7563115802c698a64918
Jahanvi Hooda <jahanvi_hooda@hotmail.com>
2018-01-29 18:19:19 +0000

    removed comments

--- staff-directory.js
+++ staff-directory.js
@@ -1,12 +1,3 @@
-/**
- *
- *  Added by Jahanvi Hooda, 10/04/17
- *  This function returns the url parameter, 
- *  and deals with multiple parameters as well
- *  Parameter is set to a variable called urlParameter
- *  Credit: http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
- *
- */
 function getURLParameter(Param) {
     var pageURL = window.location.search.substring(1);
     var variablesURL = pageURL.split('&');
@@ -19,15 +10,8 @@ function getURLParameter(Param) {
 }
 
 var urlParameter = getURLParameter('aff');
-//console.log(urlParameter);
 
-/**
- *
- *  Added by Jahanvi Hooda, 10/04/17
- *  If statements check value of urlParameter
- *  and change value of drop down menu accordingly
- *
- */
+
 if (urlParameter == 'ftf') {
     $('#faculty-filter').val('Full-time Faculty').change();
 }
@@ -39,7 +23,3 @@ if (urlParameter == 'it') {
 if (urlParameter == 'osas') {
     $('#staff-filter').val('Office of Student & Academic Services').change();
 }
-
-/**
- * Jahanvi Hooda, 10/04/17 ends
- */
